ae5bf8b3047081befa2b716420e4e3cd592d4a6f
lokesh meesala <lokeshmeesala05@gmail.com>
2026-06-30 13:38:12 +0530

    UI: Remove download symbol from View Details button on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -274,8 +274,7 @@ const Home: React.FC = () => {
                   </span>
                 </div>
                 
-                <button className={`mt-auto w-full py-3 rounded-xl font-bold flex items-center justify-center gap-2 transition-all shadow-md ${colorCode.split(' ')[1]} ${colorCode.split(' ')[2]} ${colorCode.split(' ')[4] || ''} hover:opacity-90 relative z-10`}>
-                  <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2.5} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" /></svg>
+                <button className={`mt-auto w-full py-3 rounded-xl font-bold flex items-center justify-center transition-all shadow-md ${colorCode.split(' ')[1]} ${colorCode.split(' ')[2]} ${colorCode.split(' ')[4] || ''} hover:opacity-90 relative z-10`}>
                   View Details
                 </button>
               </div>
